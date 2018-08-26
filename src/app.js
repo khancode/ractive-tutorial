834f9c9bc3a5cce481336cab3e793a0eeaebea2f
khancode <khancodegt@gmail.com>
2018-08-26 14:04:29 -0700

    step 2 - insert template variables

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import Ractive from 'ractive';
 // execute this code with the play button in the top-right corner of this pane
 const ractive = new Ractive({
   target: '#target',
-  template: '#template'
+  template: '#template',
+  data: { greeting: 'Hello', name: 'world' }
 });
-
-// throughout these tutorials, there is an output div with an id of 'target'
-// and a template script with an id of 'template'
-// you can find the output div and template script on the HTML tab
