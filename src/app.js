d527c853012e653dbdc4e7e33be192c1aed5a394
khancode <khancodegt@gmail.com>
2018-08-26 14:22:49 -0700

    step 1 - nested properties

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 import Ractive from 'ractive';
 
-console.log(Ractive);
+const ractive = new Ractive({
+    target: '#target',
+    template: '#template',
+    data: {
+        country: {
+            name: 'The UK',
+            climate: { temperature: 'cold', rainfall: 'excessive' },
+            population: 63230000,
+            capital: { name: 'London', lat: 51.5171, lon: -0.1062 }
+        }
+    }
+});
