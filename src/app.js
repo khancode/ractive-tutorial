a4b996ccd084ccab3f19a8d08277e4d22b49ae25
khancode <khancodegt@gmail.com>
2018-09-11 23:55:13 -0700

    step 1 - basic expression usage

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 import Ractive from 'ractive';
 
-console.log(Ractive);
+const ractive = new Ractive({
+    el: '#target',
+    template: '#template',
+    data: {
+        country: 'the UK',
+        population: 63230000,
+        format: num => {
+            if (num > 1000000000) return `${(num / 1000000000).toFixed(1)} billion`;
+            if (num > 1000000) return `${(num / 1000000).toFixed(1)} million`;
+            if (num > 1000) return `${Math.floor(num / 1000)} , ${(num % 1000)}`;
+            return num;
+        }
+    }
+});
+
+ractive.set({
+    country: 'China',
+    population: 1351000000
+});
