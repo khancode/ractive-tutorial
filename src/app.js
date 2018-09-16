d2b792018e167efab85643adc00644faffbb1ddc
khancode <khancodegt@gmail.com>
2018-09-16 10:23:20 -0700

    step 4 - @event usage

--- src/app.js
+++ src/app.js
@@ -2,9 +2,5 @@ import Ractive from 'ractive';
 
 const ractive = new Ractive({
     el: '#target',
-    template: '#template',
-    data: {
-        me: { cows: 0 },
-        sibling: { cows: 0 }
-    }
+    template: '#template'
 });
