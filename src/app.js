232cb3405c60f2f16af2a94bdc0ddf133d3cb31c
khancode <khancodegt@gmail.com>
2018-09-16 11:29:12 -0700

    step 1 - #if section

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 import Ractive from 'ractive';
 
-console.log(Ractive);
+const ractive = new Ractive({
+    target: '#target',
+    template: '#template',
+    data: {
+        signedIn: false,
+        notSignedIn: true
+    },
+    signIn: function () {
+        const name = prompt('Enter your username to sign in', 'ractive_fan');
+
+        ractive.set({
+            username: name,
+            signedIn: true,
+            notSignedIn: false
+        });
+    }
+});
