92d9ee92ff2df6711364a61106e14b3c2b023dea
khancode <khancodegt@gmail.com>
2018-09-16 11:13:39 -0700

    step 8 - unsubscribe, pause, resume events

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ import Ractive from 'ractive';
 
 const ractive = new Ractive({
     el: '#target',
-    template: '#template',
-    data: {
-        user: {
-            name: 'Joe'
-        }
+    template: '#template'
+});
+
+const listener = ractive.on({
+    activate: function () {
+        alert( 'Activating!');
+    },
+    deactivate: function () {
+        alert( 'Deactivating!');
+    },
+    stop: function () {
+        alert( 'No more alerts!' );
+        listener.cancel();
     }
 });
 
-ractive.on('activate', function(ctx, user) {
-    alert(`Activating ${user.name}!`);
+ractive.on({
+    toggle: function () {
+        if (listener.isSilenced()) {
+            listener.resume();
+            this.set('silenced', false);
+        } else {
+            listener.silence();
+            this.set('silenced', true);
+        }
+    }
 });
