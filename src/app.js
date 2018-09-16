ee0e2f974cf5fab8e21603b0cdd2b2773d9174b6
khancode <khancodegt@gmail.com>
2018-09-16 10:29:16 -0700

    step 5 - instance-level event system

--- src/app.js
+++ src/app.js
@@ -4,3 +4,10 @@ const ractive = new Ractive({
     el: '#target',
     template: '#template'
 });
+
+ractive.on('activate', function ( context ) {
+    // `this` is the ractive instance
+    // `context` is a context object
+    // any additional event arguments would follow `context`, if supplied
+    alert( 'Activating!' );
+});
