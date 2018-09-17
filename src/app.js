80e8e7c6b790825e8fac8612f31d4851470527d1
khancode <khancodegt@gmail.com>
2018-09-16 18:26:13 -0700

    step 2 - toggle checkboxes and radio buttons

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ const ractive = new Ractive({
     target: '#target',
     template: '#template'
 });
+
+ractive.toggle('checked');
+
+// Equivalent to:
+//   var checked = ractive.get( 'checked' );
+//   ractive.set( 'checked', !checked );
